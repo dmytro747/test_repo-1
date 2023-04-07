0f220a3edecbb75bddd0daa7428f60758dc4c647
dhryshyn <dev.dmytroh@gmail.com>
2023-04-07 17:24:40 +0300

    Updated 2

---
+++ test/index copy.tsx
@@ -0,0 +1,10 @@
+import { CompatRoute } from "react-router-dom-v5-compat";
+import React from 'react';
+console.log('test')
+const test = () => {
+  return (
+    <div>test</div>
+  )
+}
+
+export default test
