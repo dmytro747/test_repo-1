1d70d9eb80e233a0e44fb61e6e1950ee6478cce7
dhryshyn <dev.dmytroh@gmail.com>
2023-04-17 16:35:13 +0300

    Test commit

--- test/index.tsx
+++ test/index.tsx
@@ -1,4 +1,4 @@
-import { CompatRouter } from 'react-router-dom-v5-compat';
+import { useNavigate } from 'react-router-dom-v5-compat';
 import React from 'react';
 console.log('test');
 const test = () => {
