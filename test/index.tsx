6f7dbc37c3cde4e860237bf3f080d6245d6a4266
dhryshyn <dev.dmytroh@gmail.com>
2023-04-17 16:29:10 +0300

    Test commit

--- test/index.tsx
+++ test/index.tsx
@@ -1,9 +1,7 @@
+import { CompatRouter } from 'react-router-dom-v5-compat';
 import React from 'react';
-console.log('tessdfsdft')
+console.log('test');
 const test = () => {
-  return (
-    <div>test</div>
-  )
-}
-
-export default test
+	return <div>test</div>;
+};
+export default test;
